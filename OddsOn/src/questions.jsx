301bc8fba82b059b2b07d87906a4de73d888d035
Julia Johnstone <49156322+JohnstoneJ@users.noreply.github.com>
2020-12-05 21:52:39 +0100

    Project
    Mandatory group project.

---
+++ OddsOn/src/questions.jsx
@@ -0,0 +1,212 @@
+export const questions_standard = [ 
+
+"that don’t go higher than 1 to 10 while playing Odds On tonight? ", 
+
+"get bald right now?", 
+
+"sing everything you say for 30 minutes? ", 
+
+"shave a mustache and wear it for one week? ", 
+
+"do a headstand for one minute? ", 
+
+"let someone put lipstick on your lips? ", 
+
+"fill your mouth with water and try singing a song? ", 
+
+"make an obscene phone call to a random number? ", 
+
+"yell at the top of your voice? ", 
+
+"stuff ice cubes in the mouth for a minute? ", 
+
+"go out on the road and say ‘I LOVE YOU to the first person you see? ", 
+
+"speak with a fake accent for the rest of the game? ", 
+
+"soak your T-shirt in water and put them in the fridge for 20 min, and then wear it? ", 
+
+"do a handstand for one minute? ", 
+
+"switch t-shirt with the one to the right of you", 
+
+"ask a stranger to tie your shoe laces?", 
+
+"give a compliment to the person to the left of you", 
+
+"challenge one person in a thumb war?", 
+
+"jump around and sound like a frog?", 
+
+"have a dance battle with the person in front of ", 
+
+"dance to your favorite song in 30 seconds?", 
+
+"speak with a Norwegian accent during the whole game?", 
+
+"dab 15 times in 5 seconds?", 
+
+"sound like your favorite animal?", 
+
+"lick your elbow?", 
+
+"say your best pick up line?", 
+
+"sing a song from the movie ”High School Musical”?", 
+
+"lick your nose and floss at the same time?", 
+
+"that tell us a joke?", 
+
+"walk down the street in only your underwear?", 
+
+"start barking at every dog you meet. When they bark, bark back, as though you understand?", 
+
+"high-five the most strangers you can in 60 seconds?", 
+
+"bow to every stranger you meet. Tell them ”it was an honor to meet you my lord and master.”",,  
+
+"challenge one person in rock, paper, scissors?  The looser has to buy you a treat.", 
+
+" taking of your shirt and making 20 push-ups right here?", 
+
+" go to fridge and eat/drink the first thing you grab blindfolded?", 
+
+" taking 10 selfies with strangers tonight?", 
+
+" imitate your favorite celebrity for the next 30 minutes", 
+
+" not getting higher than 1 to 10 while playing this game tonight?", 
+
+" to ring the bell of a random door and ask for a banana?", 
+
+" try to break dance right here, right now", 
+
+" going down to your knees and making a proposal of marriage to the next person walking by?", 
+
+" give a hug to a stranger? (or a selected person)", 
+
+" lick a foot", 
+
+" not touching your phone for the next hour?", 
+
+" sing everything you say for 30 minutes?",  
+
+] 
+
+ export const questions_party = [ 
+
+" soak your T-shirt in water and put them in the fridge for 20 min, and then wear it? ", 
+
+" do a trick shot? ", 
+
+"make your own drink of 3 ingredients and give it to the person next to you?", 
+
+" imitate one person in the room? Everyone else guess who it is! Losers, drink five sips!", 
+
+" build a card tower?", 
+
+" challenge one person in a thumb war? Loser, drink up your glass!", 
+
+" go to your neighbor and ask them for a spare condom?", 
+
+ "challenge one person in rock, paper, scissors? Loser, drink up your glass!", 
+
+" eat a whole piece of paper?", 
+
+" sniff your friend’s socks – while they’re still wearing them?", 
+
+" kiss everyone’s knees?", 
+
+" do a catwalk? ", 
+
+" without any warning, turn to a friend and give them a French kiss", 
+
+" read your latest text message in front of everyone?", 
+
+" swap clothes with a friend, then wear their clothes for the next hour? Pretend to be that friend, too, by answering to their name.", 
+
+" send a snapchat off your little toe to someone that everyone else chooses?", 
+
+" have a dance battle with the person in front of you?", 
+
+" sip of your friends glass?", 
+
+" give away your drink to the third person sitting to your right?", 
+
+" drink everything that is in your glass?", 
+ 
+" challenge a friend in “flip the cup”?  Loser – take a sip of your drink", 
+ 
+" rap your latest text message?", 
+
+" play a song that everyone else has to guess which it is? Losers - drink up" , 
+
+" perform charades? The charades has to be a verb", 
+
+" do a shotgun a can?", 
+
+" challenge the person to the right of you in who makes the longest jump? - Loser- drink up ", 
+
+"  switch places with the person you find the most attractive?", 
+
+" switch you drink with whoever you are?", 
+
+" give a lapdance to the third person on your left side? The song is “Twinkle twinkle little star", 
+
+" go around the group saying fruits? The person who is to slow or repeats a fruit has to drink", 
+
+" do a body shot from the person next to you", 
+
+" not touching your phone for the next hour", 
+
+" let someone draw a “tattoo” with a pen on your arm?", 
+
+" licking the floor/table?", 
+
+" to sing everything you say for 30 minutes?", 
+
+" going over to this guy/girl handing over a condom and saying “you will need it later?", 
+
+" writing fuck on your right and you on your left eyelid?", 
+
+" draw a fake mustache or whiskers with a pen?", 
+
+" write your phone number on your face?", 
+
+" write your instaname on your face?", 
+
+" write your snapchat on your face?",  
+
+" call someone and if they don’t answer you have to take 1 shot? ", 
+
+" order pizza to everyone in this game?", 
+
+" challenge someone in “thee in a row”? The looser has to drink", 
+
+" answer a question that the group asks? The group has to ask the question now.", 
+
+" challenge the person to your left on a “blink-game”? The loser takes a shot. ",
+
+" go around the group saying animals? The person who is to slow or repeats an animal has to 10 sips of their own drink.", 
+
+" go around the group saying sex positions? The person who is to slow or repeats an position has to 10 sips of their own drink.", 
+
+" come up with a new sex position? Show it on the person to your right." , 
+
+" tell something about yourself that is true or false? The other players has to guess. Losers drink up!",  
+
+" tell a riddle? The person who says the right answer doesn’t have to drink. All the others take 6 sips!" ,
+
+" change clothes with a person in the game?", 
+
+" tell us a joke?", 
+
+" take a shot of tequila?",  
+
+" the person next to you draw a tattoo on your arm?", 
+
+" do a moonwalk across the room?",
+
+
+] 
